8676a6eb378b6ba872364834f6d2c06c4110f317
saud-shahzad <57951076+saud-shahzad@users.noreply.github.com>
2024-07-14 12:31:01 +0400

    Create stackbit.config.js

---
+++ stackbit.config.js
@@ -0,0 +1,15 @@
+import { ContentfulContentSource } from '@stackbit/cms-contentful'
+
+export default {
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    }),
+  ],
+  modelExtensions: {
+    // page definitions, field extensions, etc ...
+  },
+}
